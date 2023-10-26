d2566fe7731e967ed1b77c8840cf97e9d6674883
Nazar <nazar.fedorowych@gmail.com>
2023-10-26 12:00:06 +0200

    Add i18n middleware

--- app/common/middlewares/__init__.py
+++ app/common/middlewares/__init__.py
@@ -5,12 +5,15 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from app.common.middlewares.add_user import AddUserMiddleware
 from app.common.middlewares.database import DatabaseMiddleware
 from app.common.middlewares.func import ComSubMiddleware
+from app.common.middlewares.i18n import simple_locale_middleware
 
 
 def register_middlewares(dp: Dispatcher, engine: Optional[AsyncEngine] = None) -> None:
     db_middleware = DatabaseMiddleware(engine)
     com_sub_middleware = ComSubMiddleware()
     add_user_middleware = AddUserMiddleware()
+
+    simple_locale_middleware.setup(dp)
 
     dp.message.outer_middleware.register(db_middleware)
     dp.callback_query.outer_middleware.register(db_middleware)
