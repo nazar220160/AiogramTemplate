bde3e4c177df85e63424734845f23eae9a47556c
Nazar <nazar.fedorowych@gmail.com>
2023-12-14 16:08:29 +0100

    Commit

--- src/bot/utils/interactions/__init__.py
+++ src/bot/utils/interactions/__init__.py
@@ -1,5 +1,11 @@
-from app.utils.interactions.chat import Chat
+from src.bot.utils.interactions.chat import ChatMessagePagination, ChatFunctionPagination
+from src.bot.utils.interactions.pagination import InMemoryDataPagination, DataPaginationMediator, AsyncDatabaseOffsetPagination, DatabaseDataPaginationMediator
 
 __all__ = (
-    'Chat',
+    'ChatMessagePagination',
+    'ChatFunctionPagination',
+    'InMemoryDataPagination',
+    'DataPaginationMediator',
+    'AsyncDatabaseOffsetPagination',
+    'DatabaseDataPaginationMediator'
 )
